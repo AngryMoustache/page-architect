5a2997eab72b1867dd2ef5f4957f4664e82f40fc
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-22 18:32:27 +0200

    first commit

---
+++ src/PageArchitectServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AngryMoustache\PageArchitect;
+
+use Illuminate\Support\ServiceProvider;
+
+class PageArchitectServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
