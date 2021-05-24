24eea8db9dabe8460bebb8fe90f979eaf50add13
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-24 11:01:33 +0200

    Build NPM

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix')
 
 mix.disableSuccessNotifications()
-  .sass('resources/css/page-architect.scss', '../../../public/vendor/page-architect/css')
+  .sass('resources/css/page-architect.scss', 'public/css')
 
 if (mix.inProduction()) {
   mix.version()
