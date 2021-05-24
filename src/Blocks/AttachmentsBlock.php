e8ceeeacda3c47995cc5a42a7391d0559b706636
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-24 21:20:51 +0200

    Remove Temp

--- src/Blocks/AttachmentsBlock.php
+++ src/Blocks/AttachmentsBlock.php
@@ -4,35 +4,13 @@ namespace AngryMoustache\PageArchitect\Blocks;
 
 use AngryMoustache\Media\Models\Attachment;
 use AngryMoustache\Rambo\Resource\Fields\ManyAttachmentField;
-use AngryMoustache\Rambo\Resource\Fields\TabGroup;
-use AngryMoustache\Rambo\Resource\Fields\TextField;
 
 class AttachmentsBlock extends Block
 {
     public function fields()
     {
         return [
-            TabGroup::make()->tabs([
-                'general' => 'General',
-                'media' => 'Media',
-            ])->fields([
-                'general' => [
-                    TextField::make('text')
-                        ->rules('required'),
-                    TextField::make('text1')
-                        ->rules('required'),
-                    TextField::make('text2')
-                        ->rules('required'),
-                    TextField::make('text3')
-                        ->rules('required'),
-                ],
-                'media' => [
-                    ManyAttachmentField::make('attachments')
-                        ->rules('required'),
-                ]
-            ]),
-
-            ManyAttachmentField::make('attachments_1')
+            ManyAttachmentField::make('attachments')
                 ->rules('required'),
         ];
     }
