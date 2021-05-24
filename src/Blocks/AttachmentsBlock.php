b4c1b950a5e30827d949038573ff13e8b886a482
AngryMoustache <sander.vandamme11@gmail.com>
2021-05-24 20:30:43 +0200

    Temp Attachment block for testing

--- src/Blocks/AttachmentsBlock.php
+++ src/Blocks/AttachmentsBlock.php
@@ -4,13 +4,35 @@ namespace AngryMoustache\PageArchitect\Blocks;
 
 use AngryMoustache\Media\Models\Attachment;
 use AngryMoustache\Rambo\Resource\Fields\ManyAttachmentField;
+use AngryMoustache\Rambo\Resource\Fields\TabGroup;
+use AngryMoustache\Rambo\Resource\Fields\TextField;
 
 class AttachmentsBlock extends Block
 {
     public function fields()
     {
         return [
-            ManyAttachmentField::make('attachments')
+            TabGroup::make()->tabs([
+                'general' => 'General',
+                'media' => 'Media',
+            ])->fields([
+                'general' => [
+                    TextField::make('text')
+                        ->rules('required'),
+                    TextField::make('text1')
+                        ->rules('required'),
+                    TextField::make('text2')
+                        ->rules('required'),
+                    TextField::make('text3')
+                        ->rules('required'),
+                ],
+                'media' => [
+                    ManyAttachmentField::make('attachments')
+                        ->rules('required'),
+                ]
+            ]),
+
+            ManyAttachmentField::make('attachments_1')
                 ->rules('required'),
         ];
     }
